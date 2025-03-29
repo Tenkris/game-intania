2b888a27647d96cd337b17948632d3fbe912565d
Werapat <hellp002@gmail.com>
2025-03-29 21:30:16 +0700

    feat: add default button image to component

--- components/common/ButtonImage.tsx
+++ components/common/ButtonImage.tsx
@@ -1,7 +1,8 @@
 import Image, { StaticImageData } from "next/image";
 import { ReactNode } from "react";
+import longButtonImg from "@/app/assets/button/long_button.png";
 
-export default function ButtonImage({ src, alt = "button", children, onClick , type}: { src: string | StaticImageData, alt: string , children: ReactNode | string, onClick?: () => void, type?: "button" | "submit" | "reset" | undefined }) {
+export default function ButtonImage({ src = longButtonImg, alt = "button", children, onClick , type}: { src?: string | StaticImageData, alt?: string , children: ReactNode | string, onClick?: () => void, type?: "button" | "submit" | "reset" | undefined }) {
     return (
         <div className={`relative w-full`}>
             <Image src={src} width = {640} height = {640} alt={alt} className="absolute top-0 left-0 w-full h-full z-[-1] [image-rendering:pixelated]" />
