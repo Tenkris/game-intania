648e12c453d2c86e37101d744a75a6ac478e6818
WinSukon <jakrinsukon@gmail.com>
2025-03-29 15:41:57 +0700

    fix: use client

--- app/gift/page.tsx
+++ app/gift/page.tsx
@@ -1,53 +1,11 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { UpdateUserBody, User } from "@/types/user";
-import { updateUser, getMyUser } from "@/utils/api/user";
-import { useRouter } from "next/navigation";
-const mockUpgradables = [
-  { label: "+ 20 ATK", attribute: "attack", value: 20 },
-  { label: "+ 20 DEF", attribute: "defense", value: 20 },
-  { label: "+ 20 SPD", attribute: "speed", value: 20 },
-  { label: "+ 20% Critical", attribute: "critical", value: 20 },
-];
-const Gift = () => {
-  const router = useRouter();
-  const handleUpdateUser = async (field: keyof User, value: number) => {
-    const me = await getMyUser();
-    if (me) {
-      console.log(me);
-      if (typeof me[field] === "number") {
-        const updateData: UpdateUserBody = {
-          [field]: me[field] + value,
-        };
-        const response = await updateUser(updateData);
-        if (response) {
-          router.push("/end");
-        }
-      } else {
-        console.error(`Field '${field}' is not a number or is undefined`);
-      }
-    }
-  };
+import GiftButtons from "@/components/gift/GiftButtons";
 
+const Gift = () => {
   return (
     <div className="w-full h-full flex justify-center items-center ">
       <div className="bg-white shadow-md rounded-lg p-4 opacity-80 w-96 items-center justify-center ">
         <h1 className="text-center pb-3 m-3 text-xl">Choose your power up</h1>
-        <div className="grid grid-cols-2 gap-4">
-          {mockUpgradables.map((item) => (
-            <Button
-              key={item.attribute}
-              className="flex"
-              variant={"default"}
-              size={"lg"}
-              onClick={() =>
-                handleUpdateUser(item.attribute as keyof User, item.value)
-              }
-            >
-              {item.label}
-            </Button>
-          ))}
-        </div>
+        <GiftButtons />
       </div>
     </div>
   );
---
+++ components/gift/GiftButtons.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { UpdateUserBody, User } from "@/types/user";
+import { updateUser, getMyUser } from "@/utils/api/user";
+import { useRouter } from "next/navigation";
+
+const mockUpgradables = [
+  { label: "+ 20 ATK", attribute: "attack", value: 20 },
+  { label: "+ 20 DEF", attribute: "defense", value: 20 },
+  { label: "+ 20 SPD", attribute: "speed", value: 20 },
+  { label: "+ 20% Critical", attribute: "critical", value: 20 },
+];
+const GiftButtons = () => {
+  const router = useRouter();
+
+  const handleUpdateUser = async (field: keyof User, value: number) => {
+    const me = await getMyUser();
+    
+    if (me) {
+      console.log(me);
+      if (typeof me[field] === "number") {
+        const updateData: UpdateUserBody = {
+          [field]: me[field] + value,
+        };
+        const response = await updateUser(updateData);
+        console.log(response)
+        if (response) {
+          router.push("/end");
+        }
+      } else {
+        console.error(`Field '${field}' is not a number or is undefined`);
+      }
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {mockUpgradables.map((item) => (
+        <Button
+          key={item.attribute}
+          className="flex"
+          variant={"default"}
+          size={"lg"}
+          onClick={() =>
+            handleUpdateUser(item.attribute as keyof User, item.value)
+          }
+        >
+          {item.label}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default GiftButtons;
