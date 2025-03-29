5e058af3874d56fb5c582090ba946cbe3052fc58
WinSukon <jakrinsukon@gmail.com>
2025-03-29 11:37:06 +0700

    feat: mock dynamic gift

--- app/gift/page.tsx
+++ app/gift/page.tsx
@@ -3,7 +3,12 @@ import { Button } from "@/components/ui/button";
 import { UpdateUserBody } from "@/types/user";
 import { updateUser } from "@/utils/api/user";
 import { useRouter } from "next/navigation";
-
+const mockUpgradables = [
+  { label: "+ 20 ATK", attribute: "attack", value: 20 },
+  { label: "+ 20 DEF", attribute: "defense", value: 20 },
+  { label: "+ 20 SPD", attribute: "speed", value: 20 },
+  { label: "+ 20% Critical", attribute: "critical", value: 20 },
+];
 const Gift = () => {
   const router = useRouter();
   const handleUpdateUser = async (updateData: UpdateUserBody) => {
@@ -18,38 +23,17 @@ const Gift = () => {
       <div className="bg-white shadow-md rounded-lg p-4 opacity-80 w-96 items-center justify-center ">
         <h1 className="text-center pb-3 m-3 text-xl">Choose your power up</h1>
         <div className="grid grid-cols-2 gap-4">
-          <Button
-            className="flex "
-            variant={"default"}
-            size={"lg"}
-            onClick={() => handleUpdateUser({ attack: 20 })}
-          >
-            + 20 ATK
-          </Button>
-          <Button
-            className="flex "
-            variant={"default"}
-            size={"lg"}
-            onClick={() => handleUpdateUser({ defense: 20 })}
-          >
-            + 20 DEF
-          </Button>
-          <Button
-            className="flex "
-            variant={"default"}
-            size={"lg"}
-            onClick={() => handleUpdateUser({ speed: 20 })}
-          >
-            + 20 SPD
-          </Button>
-          <Button
-            className="flex "
-            variant={"default"}
-            size={"lg"}
-            onClick={() => handleUpdateUser({ critical: 20 })}
-          >
-            + 20% Armor pierce
-          </Button>
+          {mockUpgradables.map((item) => (
+            <Button
+              key={item.attribute}
+              className="flex"
+              variant={"default"}
+              size={"lg"}
+              onClick={() => handleUpdateUser({ [item.attribute]: item.value })}
+            >
+              {item.label}
+            </Button>
+          ))}
         </div>
       </div>
     </div>
