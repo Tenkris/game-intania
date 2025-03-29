cdee243801c3e3d962b6ec13f402652ea6236fca
JusJira <jus.jira@gmail.com>
2025-03-29 14:56:16 +0700

    chore: lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import MusicPlayer from "@/components/sound/music-player";
 import { logout } from "@/utils/api/auth";
 import { getMyUser } from "@/utils/api/user";
 import Image from "next/image";
@@ -5,47 +6,42 @@ import Link from "next/link";
 import longButtonImg from "@/app/assets/button/long_button.png";
 
 export default async function HomePage() {
-
   // Check if user is logged in
   const userInfo = await getMyUser();
-
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-5">
       <div className="card w-96 items-center justify-center">
-        {
-          userInfo ? (
-            <Link href="/game" className="button">Play</Link>
-          ) : (
-            <>
-              <Link href="/login" className="button">Login</Link>
-              <Link href="/register" className="button">Register</Link>
-            </>
-            
-          )
-        }
-        
-        
-        {
-          userInfo && (
-            <>
-              <Link href="/leaderboard" className="button">Leaderboard</Link>
-              <Link href="/history" className="button">History</Link>
-              <button className="button" onClick={logout}>Log Out</button>
-            </>
-          )
-        }
-        
-        {/* <button className="aspect-[1822/449] h-10">
-          <Image src={longButtonImg} alt="Hi" className=""/>
-          <div className="">
-            Hi
-          </div>
-        </button> */}
-        
+        {userInfo ? (
+          <Link href="/game" className="button">
+            Play
+          </Link>
+        ) : (
+          <>
+            <Link href="/login" className="button">
+              Login
+            </Link>
+            <Link href="/register" className="button">
+              Register
+            </Link>
+          </>
+        )}
+
+        <Link href="/leaderboard" className="button">
+          Leaderboard
+        </Link>
+        {userInfo && (
+          <>
+            <Link href="/history" className="button">
+              History
+            </Link>
+            <button className="button" onClick={logout}>
+              Log Out
+            </button>
+          </>
+        )}
       </div>
-      
-      
+      <MusicPlayer src="https://s3.imjustin.dev/hackathon/2020-03-22_-_A_Bit_Of_Hope_-_David_Fesliyan.mp3" />
     </div>
   );
 }
