635c62885f58f71a5b8e3137032afb2c66ebd6b4
JusJira <jus.jira@gmail.com>
2025-03-29 14:56:16 +0700

    chore: lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,45 @@
+import MusicPlayer from "@/components/sound/music-player";
 import { logout } from "@/utils/api/auth";
 import { getMyUser } from "@/utils/api/user";
 import Link from "next/link";
 
 export default async function HomePage() {
-
   // Check if user is logged in
   const userInfo = await getMyUser();
-
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-5">
       <div className="card w-96 items-center justify-center">
-        {
-          userInfo ? (
-            <Link href="/game" className="button">Play</Link>
-          ) : (
-            <>
-              <Link href="/login" className="button">Login</Link>
-              <Link href="/register" className="button">Register</Link>
-            </>
-            
-          )
-        }
-        
-        <Link href="/leaderboard" className="button">Leaderboard</Link>
-        {
-          userInfo && (
-            <>
-              <Link href="/history" className="button">History</Link>
-              <button className="button" onClick={logout}>Log Out</button>
-            </>
-          )
-        }
-        
+        {userInfo ? (
+          <Link href="/game" className="button">
+            Play
+          </Link>
+        ) : (
+          <>
+            <Link href="/login" className="button">
+              Login
+            </Link>
+            <Link href="/register" className="button">
+              Register
+            </Link>
+          </>
+        )}
+
+        <Link href="/leaderboard" className="button">
+          Leaderboard
+        </Link>
+        {userInfo && (
+          <>
+            <Link href="/history" className="button">
+              History
+            </Link>
+            <button className="button" onClick={logout}>
+              Log Out
+            </button>
+          </>
+        )}
       </div>
-      
-      
+      <MusicPlayer src="https://s3.imjustin.dev/hackathon/2020-03-22_-_A_Bit_Of_Hope_-_David_Fesliyan.mp3" />
     </div>
   );
 }
