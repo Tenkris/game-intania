b0fcdad87647623e9c04ab99460698e709f3feb6
JusJira <jus.jira@gmail.com>
2025-03-30 12:38:57 +0700

    style: css

--- app/tutorial/page.tsx
+++ app/tutorial/page.tsx
@@ -10,11 +10,36 @@ import ButtonImage from "@/components/common/ButtonImage";
 
 export default function TutorialPage() {
   const steps = [
-    { step: 1, title: "Game Start", description: "The game starts with a question that you need to answer.", image: tutorial1Image },
-    { step: 2, title: "Critical Damage", description: "If you answer correctly and fast, press the spacebar at the right time to deal critical damage to the boss.", image: tutorial2Image },
-    { step: 3, title: "Guarding", description: "Press spacebar repeatedly to guard the boss from attacks.", image: tutorial3Image },
-    { step: 4, title: "Gift Selection", description: "After defeating the boss, choose your gift and proceed to the next question." },
-    { step: 5, title: "Game Over", description: "If your health drops to zero, you lose the game." }
+    {
+      step: 1,
+      title: "Game Start",
+      description: "The game starts with a question that you need to answer.",
+      image: tutorial1Image,
+    },
+    {
+      step: 2,
+      title: "Critical Damage",
+      description:
+        "If you answer correctly and fast, press the spacebar at the right time to deal critical damage to the boss.",
+      image: tutorial2Image,
+    },
+    {
+      step: 3,
+      title: "Guarding",
+      description: "Press spacebar repeatedly to guard the boss from attacks.",
+      image: tutorial3Image,
+    },
+    {
+      step: 4,
+      title: "Gift Selection",
+      description:
+        "After defeating the boss, choose your gift and proceed to the next question.",
+    },
+    {
+      step: 5,
+      title: "Game Over",
+      description: "If your health drops to zero, you lose the game.",
+    },
   ];
 
   const [currentStep, setCurrentStep] = useState<number>(0);
@@ -36,31 +61,31 @@ export default function TutorialPage() {
       <div className="card w-11/12 lg:w-2/5 items-center justify-center">
         {/* Header */}
         <div className="flex justify-between items-center w-full p-2 lg:p-4">
-            <BackButton href="/" />
-            <h1 className="text-lg font-bold mx-auto">Tutorial</h1>
-            <div className="w-6 h-6"></div>
+          <BackButton href="/" />
+          <h1 className="text-lg font-bold mx-auto">Tutorial</h1>
+          <div className="w-6 h-6"></div>
         </div>
 
         {/* Tutorial Step */}
         <div className="flex flex-col justify-center w-full px-4 py-2 gap-2 text-xs">
-          <span className="font-bold">Step {steps[currentStep].step}: {steps[currentStep].title}</span>
+          <span className="font-bold">
+            Step {steps[currentStep].step}: {steps[currentStep].title}
+          </span>
           <p className="text-gray-700">{steps[currentStep].description}</p>
-          {
-            (steps[currentStep].image) && (
-              <div className="w-full flex justify-center">
-                <Image
-                  src={steps[currentStep].image}
-                  alt={`Tutorial Step ${steps[currentStep].step}`}
-                  className="w-auto h-60 shadow-md mt-2"
-                />
-              </div>
-            )
-          }
+          {steps[currentStep].image && (
+            <div className="w-full flex justify-center">
+              <Image
+                src={steps[currentStep].image}
+                alt={`Tutorial Step ${steps[currentStep].step}`}
+                className="w-auto h-60 shadow-md mt-2"
+              />
+            </div>
+          )}
         </div>
 
         {/* Navigation */}
         <div className="w-full border-b border-gray-300"></div>
-        <div className="w-full flex justify-between items-center px-4 py-2 text-xs">
+        <div className="w-full flex justify-between items-center px-4 py-2 text-xs z-30">
           {/* <button
             className={`btn ${currentStep > 0 ? "btn-primary" : "btn-disabled opacity-50"}`}
             onClick={goToPreviousStep}
@@ -68,7 +93,7 @@ export default function TutorialPage() {
           >
             Previous
           </button> */}
-          <ButtonImage onClick={goToPreviousStep} disabled={currentStep === 0} >
+          <ButtonImage onClick={goToPreviousStep} disabled={currentStep === 0}>
             Previous
           </ButtonImage>
           {/* <button
@@ -78,7 +103,10 @@ export default function TutorialPage() {
           >
             Next
           </button> */}
-          <ButtonImage onClick={goToNextStep} disabled={currentStep === steps.length - 1} >
+          <ButtonImage
+            onClick={goToNextStep}
+            disabled={currentStep === steps.length - 1}
+          >
             Next
           </ButtonImage>
         </div>
